ef3fdcd38d144c9784b5f9df691d847d3c0e899b
Indrek <inzaheinmets@gmail.com>
2022-01-22 20:33:19 +0000

    updates and randFibreGen Changed

--- FibreMicrostructure/RandomPointGen.py
+++ FibreMicrostructure/RandomPointGen.py
@@ -1,19 +1,59 @@
+import matplotlib.pyplot as plt
+from math import sqrt
+from math import pi
+import random
 
-fibre_diameter = 20
-fibre_count = 16
-fibre_spacing_buffer = 2
-RVE_size = 6 * fibre_diameter
+# Find fibre count based on volume fraction:
+def findFibreCount(fibre_diameter, fibre_volFraction, RVE_size):
+    total_vol = RVE_size**3
+    single_fibre_vol = pi * ((fibre_diameter / 2)**2) * RVE_size
+    total_fibre_vol = fibre_volFraction * total_vol
+    fibre_count = round(total_fibre_vol / single_fibre_vol, 0)
+    actual_volFraction = round((single_fibre_vol * fibre_count) / total_vol, 4)
+    return fibre_count, actual_volFraction
 
 # Random fibre position creation:
-point_lst = []
-for i in range(fibre_count):
-    x = random.randrange(-(RVE_size / 2), (RVE_size / 2))
-    y = random.randrange(-(RVE_size / 2), (RVE_size / 2))
-    point_lst.append((x, y))
+def randFibrePos(fibre_diameter, fibre_count, fibre_spacing_buffer, RVE_size):
+    point_lst = []
+    point_lst.append((random.randrange(-int(RVE_size / 2), int(RVE_size / 2)), random.randrange(-int(RVE_size / 2), int(RVE_size / 2))))
 
+    while len(point_lst) < fibre_count:
+        for c, point in enumerate(point_lst):
+            x, y = random.randrange(-int(RVE_size / 2), int(RVE_size / 2)), random.randrange(-int(RVE_size / 2), int(RVE_size / 2))
+            x1, y1 = point
+            dx, dy = abs(x1 - x), abs(y1 - y)
+            if (fibre_diameter / 2) + fibre_spacing_buffer < sqrt((dx**2) + (dy**2)):
+                if len(point_lst) < fibre_count:
+                    point_lst.append((x, y))
+    return point_lst
+
+if __name__ == '__main__':
+    # Variables:
+    fibre_diameter = 20.0
+    fibre_volFraction = 0.35
+    fibre_spacing_buffer = 2.0
+    RVE_size = 6 * fibre_diameter
+
+    fibre_count, actual_volFraction = findFibreCount(fibre_diameter, fibre_volFraction, RVE_size)
+    point_lst = randFibrePos(fibre_diameter, fibre_count, fibre_spacing_buffer, RVE_size)
+
+    print(f'Fibre count for a given fibre volume fraction of {fibre_volFraction}: {fibre_count}')
+    print(f'With {fibre_count} fibres, the actual fibre volume fraction will be: {actual_volFraction}')
+    print(f'Points: {point_lst}')
+
+    x_lst = []
+    y_lst = []
     for c, point in enumerate(point_lst):
-        x1, y1 = point_lst[c]
-        dx, dy = abs(x1 - x), abs(y1 - y)
-        if ((fibre_diameter / 2) + fibre_spacing_buffer) <= (sqrt((dx**2) + (dy**2))):
-            a.Instance(name='Fibre-' + str(i + 1), part=p, dependent=ON)
-            a.translate(instanceList=('Fibre-' + str(i + 1), ), vector=(x, y, 0.0))
+        x_lst.append(point[0])
+        y_lst.append(point[1])
+
+w = 10
+d = 70
+plt.figure(figsize=(w, w), dpi=d)
+plt.axis([-60, 60, -60, 60])
+size = 2000
+
+plt.scatter(x_lst, y_lst, s=size)
+plt.show()
+
+# Tee graph korda (oiged suurused tappidel)
--- utills/ModelVolFractions.py
+++ utills/ModelVolFractions.py
@@ -1,23 +1,23 @@
-# Volume values from the abaqus models
+# Volume values from the Abaqus models (total_vol, matrix_vol)
+volumes = {'Plain': (5637.79, 3628.08),
+           'Basket': (5740.60, 3761.03),
+           'Mock-Leno': (5680.39, 3683.53),
+           'Satin': (5760.88, 3784.17),
+           'Twill': (5785.66, 3794.70),
+           'Extra': (5850.77, 3859.38)}
 
-# Plain:
-total_vol = 5637.79
-matrix_vol = 3628.08
 
-# Basket:
-total_vol = 5740.60
-matrix_vol = 3761.03
-
-# Mock-Leno:
-total_vol = 5680.39
-matrix_vol = 3683.53
-
-def find_volume_fraction(total_vol, matrix_vol):
+def find_volume_fraction(volumes, model):
+    total_vol, matrix_vol = volumes[model]
     fiber_vol = total_vol - matrix_vol
     vf_fibres = round(fiber_vol / total_vol, 4)
     vf_matrix = round(matrix_vol / total_vol, 4)
     return vf_fibres, vf_matrix
 
-VolFraction_Fibres, VolFraction_Matrix = find_volume_fraction(total_vol, matrix_vol)
+
+# Enter the name of the model (Plain, Basket, Mock-Leno, Satin, Twill, Extra):
+model = 'Mock-Leno'
+
+VolFraction_Fibres, VolFraction_Matrix = find_volume_fraction(volumes, model)
 print(f"Volume Fraction of CF Fibers: {VolFraction_Fibres * 100}%")
 print(f"Volume Fraction of Resin Matrix: {VolFraction_Matrix * 100}%")
