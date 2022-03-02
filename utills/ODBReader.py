a9ed91a7e4913be131de4efaefff0879211a6136
Indrek <inzaheinmets@gmail.com>
2022-03-02 23:14:19 +0000

    ODB reader fix

--- utills/ODBReader.py
+++ utills/ODBReader.py
@@ -1,90 +1,69 @@
 from job import *
 from odbAccess import openOdb
 import odbAccess
+import csv
 print('Running ODB reader...')
 
 
-def readODB(path, job_list):
-    isFirstIP = True
+def matCol(i, matrix):
+    return [row[i] for row in matrix]
+
+
+def readODB(path, job_list, RVE_size):
     for c, job in enumerate(job_list):
 
-        odbpath = path + job + '.odb'
-        odb = openOdb(odbpath)
+        odb = openOdb(path + job + '.odb')
+        keys = odb.steps['StaticAnalysis'].historyRegions.keys()
 
-        allIPs = odb.steps['StaticAnalysis'].historyRegions.keys()
+        # Reactions:
+        frp = odb.steps['StaticAnalysis'].historyRegions[keys[1]]
+        hOut_RF1 = frp.historyOutputs['RF1'].data
+        hOut_RF2 = frp.historyOutputs['RF2'].data
+        hOut_RF3 = frp.historyOutputs['RF3'].data
 
-        total_load = 0.0
+        time_vals = matCol(0, hOut_RF1)
+        RF1_vals = matCol(1, hOut_RF1)
+        RF2_vals = matCol(1, hOut_RF2)
+        RF3_vals = matCol(1, hOut_RF3)
 
-        for integrationPoint in allIPs:
+        # Displacements:
+        crp = odb.steps['StaticAnalysis'].historyRegions[keys[2]]
+        hOut_U1 = crp.historyOutputs['U1'].data
+        hOut_U2 = crp.historyOutputs['U2'].data
+        hOut_U3 = crp.historyOutputs['U3'].data
 
-            if isFirstIP:
-                isFirstIP = False
-                continue
+        U1_vals = matCol(1, hOut_U1)
+        U2_vals = matCol(1, hOut_U2)
+        U3_vals = matCol(1, hOut_U3)
 
-            t = odb.steps['StaticAnalysis'].historyRegions[integrationPoint]
+        # Stresses & Strains:
+        stressX = [rf / RVE_size**2 for rf in RF1_vals]
+        stressY = [rf / RVE_size**2 for rf in RF2_vals]
+        stressZ = [rf / RVE_size**2 for rf in RF3_vals]
+        strainX = [u / RVE_size for u in U1_vals]
+        strainY = [u / RVE_size for u in U2_vals]
+        strainZ = [u / RVE_size for u in U3_vals]
 
-            hOut_U1 = t.historyOutputs['U1'].data
-            hOut_U2 = t.historyOutputs['U2'].data
-            hOut_U3 = t.historyOutputs['U3'].data
-            hOut_RF1 = t.historyOutputs['RF1'].data
-            hOut_RF2 = t.historyOutputs['RF2'].data
-            hOut_RF3 = t.historyOutputs['RF3'].data
-
-            def matCol(i, matrix):
-                return [row[i] for row in matrix]
-
-            U1_vals = matCol(1, hOut_U1)
-            U2_vals = matCol(1, hOut_U2)
-            U3_vals = matCol(1, hOut_U3)
-            RF1_vals = matCol(1, hOut_RF1)
-            RF2_vals = matCol(1, hOut_RF2)
-            RF3_vals = matCol(1, hOut_RF3)
-            time_vals = matCol(0, hOut_RF1)
-
-            # Open text file and write results:
-            sortie = open('Results - ' + job + '.csv', 'w')
-
-            sortie.write('Results from ' + job + ': \n')
-            sortie.write('\nU1: ' + str(U1_vals))
-            sortie.write('\n\nU2: ' + str(U2_vals))
-            sortie.write('\n\nU3: ' + str(U3_vals))
-            sortie.write('\n\nRF1: ' + str(RF1_vals))
-            sortie.write('\n\nRF2: ' + str(RF2_vals))
-            sortie.write('\n\nRF3: ' + str(RF3_vals))
-            sortie.write('\n\nTime: ' + str(time_vals))
-
-        isFirstIP = True
+        # Open file and write results:
+        with open('Results - ' + job + '.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow('Results from ' + job + ':')
+            writer.writerow(['Time', 'RF1', 'RF2', 'RF3', 'U1', 'U2', 'U3', 'StressX', 'StressY', 'StressZ', 'StrainX', 'StrainY', 'StrainZ'])
+            for c, time in enumerate(time_vals):
+                writer.writerow([time, RF1_vals[c], RF2_vals[c], RF3_vals[c], U1_vals[c], U2_vals[c], U3_vals[c],
+                                 stressX[c], stressY[c], stressZ[c], strainX[c], strainY[c], strainZ[c]])
         odb.close()
-        sortie.close()
-    return U1_vals, U2_vals, U3_vals, RF1_vals, RF2_vals, RF3_vals
 
 
-# Job lists:
-test_jobs = ['TestODB1', 'TestODB2', 'TestODB3']
-
+############################# VARIABLES ###################################
 rve_jobs = ['LongitudinalTensionAnalysis', 'LongitudinalCompressionAnalysis', 'LongitudinalShearAnalysis',
             'TransverseSideTensionAnalysis', 'TransverseSideCompressionAnalysis', 'TransverseSideShearAnalysis',
             'TransverseTopTensionAnalysis', 'TransverseTopCompressionAnalysis', 'TransverseTopShearAnalysis']
-
 comp_jobs = ['TensionAnalysis', 'CompressionAnalysis', 'ShearAnalysis']
-
 path = './ODBData/'
-
-U1_vals, U2_vals, U3_vals, RF1_vals, RF2_vals, RF3_vals = readODB(path, test_jobs)
-
 RVE_size = 120
-e1, e2, e3 = [], [], []
 
-for u1 in U1_vals:
-    e1.append(u1 / RVE_size)
-
-for u2 in U2_vals:
-    e2.append(u2 / RVE_size)
-
-for u3 in U3_vals:
-    e3.append(u3 / RVE_size)
-
-print(e1, e2, e3)
+readODB(path, rve_jobs, RVE_size)
 
 # End of script:
 print('*************************')
