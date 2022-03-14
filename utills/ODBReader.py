8ce3af2456bdbe8931d5a9862beb126ce1927a9e
Indrek <inzaheinmets@gmail.com>
2022-03-14 16:55:23 +0000

    readerChanges

--- utills/ODBReader.py
+++ utills/ODBReader.py
@@ -1,5 +1,6 @@
 from job import *
 from odbAccess import openOdb
+import numpy as np
 import csv
 print('Running ODB reader...')
 
@@ -35,6 +36,12 @@ def readODB(path, job_list, RVE_size):
         U2_vals = matCol(1, hOut_U2)
         U3_vals = matCol(1, hOut_U3)
 
+        # Find primary loading axis:
+        reactions = [RF1_vals, RF2_vals, RF3_vals]
+        for c, reaction in enumerate(reactions):
+            if reaction[-1] != 0.0:
+                loading_direction = c + 1
+
         # Stresses & Strains:
         stressX = [rf / RVE_size**2 for rf in RF1_vals]
         stressY = [rf / RVE_size**2 for rf in RF2_vals]
@@ -43,15 +50,33 @@ def readODB(path, job_list, RVE_size):
         strainY = [u / RVE_size for u in U2_vals]
         strainZ = [u / RVE_size for u in U3_vals]
 
-        # Open file and write results:
-        with open('Results - ' + job + '.csv', 'w') as csvfile:
+        # Youngs/Shear Modulus and Poissons Ratio:
+        if loading_direction == 1:
+            E1 = (stressX[-1] - stressX[0]) / (strainX[-1] - strainX[0])
+            P12 = (strainY[-1] - strainY[0]) / (strainX[-1] - strainX[0])
+            P13 = (strainZ[-1] - strainZ[0]) / (strainX[-1] - strainX[0])
+
+        elif loadin_direction == 2:
+            E2 = (stressY[-1] - stressY[0]) / (strainY[-1] - strainY[0])
+            P12 = (strainY[-1] - strainY[0]) / (strainX[-1] - strainX[0])
+            P23 = (strainZ[-1] - strainZ[0]) / (strainY[-1] - strainY[0])
+
+        elif loadin_direction == 3:
+            E2 = (stressZ[-1] - stressZ[0]) / (strainZ[-1] - strainZ[0])
+            P13 = (strainZ[-1] - strainZ[0]) / (strainX[-1] - strainX[0])
+            P23 = (strainZ[-1] - strainZ[0]) / (strainY[-1] - strainY[0])
+
+        # Open file and write simulation data:
+        with open('Results - ' + job + '.csv', 'wb') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(['Results from ' + job + ':'])
             writer.writerow(['Time', 'RF1', 'RF2', 'RF3', 'U1', 'U2', 'U3', 'StressX', 'StressY', 'StressZ', 'StrainX', 'StrainY', 'StrainZ'])
             for c, time in enumerate(time_vals):
                 writer.writerow([time, RF1_vals[c], RF2_vals[c], RF3_vals[c], U1_vals[c], U2_vals[c], U3_vals[c],
                                  stressX[c], stressY[c], stressZ[c], strainX[c], strainY[c], strainZ[c]])
+
         odb.close()
+    return E1, E2, E3, P12, P13, P23
 
 
 ############################# VARIABLES ###################################
@@ -62,7 +87,15 @@ comp_jobs = ['TensionAnalysis', 'CompressionAnalysis', 'ShearAnalysis']
 path = './ODBData/'
 RVE_size = 120
 
-readODB(path, rve_jobs, RVE_size)
+E1, E2, E3, P12, P13, P23 = readODB(path, rve_jobs, RVE_size)
+
+# Open file and write material properties:
+with open('MaterialProperties.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['MaterialProperties:'])
+    writer.writerow(['E1', 'E2', 'E3', 'P12', 'P13', 'P23'])
+    for c, time in enumerate(time_vals):
+        writer.writerow([E1, E2, E3, P12, P13, P23])
 
 # End of script:
 print('*************************')
