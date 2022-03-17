2e1eafad6b1ba0750702866ade606cd972144e5f
Indrek <inzaheinmets@gmail.com>
2022-03-17 19:30:20 +0000

    readerUpdate

--- utills/ODBReader.py
+++ utills/ODBReader.py
@@ -19,51 +19,36 @@ def findStrain(disp_vals, RVE_size):
 
 def caseAxis(RF1, RF2, RF3, U1, U2, U3):
     loadCase, loadAxis = '', 0
-
     # Find load case:
-    # if U1[-1] or U2[-1] or U3[-1] == 0:
-    #     loadCase = 'shear'
-    # elif RF1[-1] or RF2[-1] or RF3[-1] < 0:
-    #     loadCase = 'compression'
-    # elif RF1[-1] or RF2[-1] or RF3[-1] > 0:
-    #     loadCase = 'tension'
-
     if U1[-1] == 0 or U2[-1] == 0 or U3[-1] == 0:
         loadCase = 'shear'
     elif RF1[-1] < 0 or RF2[-1] < 0 or RF3[-1] < 0:
         loadCase = 'compression'
     elif RF1[-1] > 0 or RF2[-1] > 0 or RF3[-1] > 0:
         loadCase = 'tension'
-
     # Find Primary loading axis:
-    # if 0 < RF1[-1] < 0:
-    #     loadAxis = 1
-    # elif 0 < RF2[-1] < 0:
-    #     loadAxis = 2
-    # elif 0 < RF3[-1] < 0:
-    #     loadAxis = 3
-
-    if RF1[-1] < 0 or RF[-1] > 0:
+    if RF1[-1] < 0 or RF1[-1] > 0:
         loadAxis = 1
     elif RF2[-1] < 0 or RF2[-1] > 0:
         loadAxis = 2
     elif RF3[-1] < 0 or RF3[-1] > 0:
         loadAxis = 3
-
     return loadCase, loadAxis
 
 
 def readODB(path, job_list, RVE_size):
     for c, job in enumerate(job_list):
+        print(job)
 
         odb = openOdb(path + job + '.odb')
         keys = odb.steps['StaticAnalysis'].historyRegions.keys()
 
+        # X-axis changed to be longitudinal fibre direction
         # Reactions:
         frp = odb.steps['StaticAnalysis'].historyRegions[keys[1]]
-        hOut_RF1 = frp.historyOutputs['RF1'].data
+        hOut_RF1 = frp.historyOutputs['RF3'].data
         hOut_RF2 = frp.historyOutputs['RF2'].data
-        hOut_RF3 = frp.historyOutputs['RF3'].data
+        hOut_RF3 = frp.historyOutputs['RF1'].data
 
         time_vals = matCol(0, hOut_RF1)
         RF1_vals = matCol(1, hOut_RF1)
@@ -72,9 +57,9 @@ def readODB(path, job_list, RVE_size):
 
         # Displacements:
         crp = odb.steps['StaticAnalysis'].historyRegions[keys[2]]
-        hOut_U1 = crp.historyOutputs['U1'].data
+        hOut_U1 = crp.historyOutputs['U3'].data
         hOut_U2 = crp.historyOutputs['U2'].data
-        hOut_U3 = crp.historyOutputs['U3'].data
+        hOut_U3 = crp.historyOutputs['U1'].data
         odb.close()
 
         U1_vals = matCol(1, hOut_U1)
@@ -85,47 +70,66 @@ def readODB(path, job_list, RVE_size):
         stressX, stressY, stressZ = findStress(RF1_vals, RVE_size), findStress(RF2_vals, RVE_size), findStress(RF3_vals, RVE_size)
         strainX, strainY, strainZ = findStrain(U1_vals, RVE_size), findStrain(U2_vals, RVE_size), findStrain(U3_vals, RVE_size)
 
+        # REMOVE:
+        # with open('ResultsFrom' + job + '.csv', 'wb') as csvfile:
+            # writer = csv.writer(csvfile)
+            # writer.writerow(['Results from ' + job + ':'])
+            # writer.writerow(['Time', 'RF1', 'RF2', 'RF3', 'U1', 'U2', 'U3'])
+            # for i, time in enumerate(time_vals):
+                # writer.writerow([time, RF1_vals[i], RF2_vals[i], RF3_vals[i], U1_vals[i], U2_vals[i], U3_vals[i]])
+
         # Material Properties Calculations:
         loadCase, loadAxis = caseAxis(RF1_vals, RF2_vals, RF3_vals, U1_vals, U2_vals, U3_vals)
+
+        # REPLACE END VALUES WITH SLOPES WHEN EVERYTHING IS WORKING
         if loadCase == 'tension':
             if loadAxis == 1:
-                E1_tens = ((stressZ[-1] - stressZ[0]) / (strainZ[-1] - strainZ[0])) / 1e9
-                P12_tens = ((strainX[-1] - strainX[0]) / (strainZ[-1] - strainZ[0]))
+                E1_tens = (stressX[-1] / strainX[-1]) / 1e9
+                P12_tens = -(strainY[-1] / strainX[-1])
+                print('TensX')
 
             elif loadAxis == 2:
-                E2_tens = ((stressX[-1] - stressX[0]) / (strainX[-1] - strainX[0])) / 1e9
-                P13_tens = ((strainY[-1] - strainY[0]) / (strainZ[-1] - strainZ[0]))
+                E2_tens = (stressY[-1] / strainY[-1]) / 1e9
+                P23_tens = -(strainZ[-1] / strainY[-1])
+                print('TensY')
 
             elif loadAxis == 3:
-                E3_tens = ((stressY[-1] - stressY[0]) / (strainY[-1] - strainY[0])) / 1e9
-                P23_tens = ((strainX[-1] - strainX[0]) / (strainZ[-1] - strainZ[0]))
+                E3_tens = (stressZ[-1] / strainZ[-1]) / 1e9
+                P13_tens = -(strainX[-1] / strainZ[-1])
+                print('TensZ')
 
         elif loadCase == 'compression':
             if loadAxis == 1:
-                E1_comp = -((stressZ[-1] - stressZ[0]) / (strainZ[-1] - strainZ[0])) / 1e9
-                P12_comp = -((strainX[-1] - strainX[0]) / (strainZ[-1] - strainZ[0]))
+                E1_comp = (stressX[-1] / strainX[-1]) / 1e9
+                P12_comp = -(strainY[-1] / strainX[-1])
+                print('CompX')
 
             elif loadAxis == 2:
-                E2_comp = -((stressX[-1] - stressX[0]) / (strainX[-1] - strainX[0])) / 1e9
-                P13_comp = -((strainY[-1] - strainY[0]) / (strainZ[-1] - strainZ[0]))
+                E2_comp = (stressY[-1] / strainY[-1]) / 1e9
+                P23_comp = -(strainZ[-1] / strainY[-1])
+                print('CompY')
 
             elif loadAxis == 3:
-                E3_comp = -((stressY[-1] - stressY[0]) / (strainY[-1] - strainY[0])) / 1e9
-                P23_comp = -((strainX[-1] - strainX[0]) / (strainY[-1] - strainY[0]))
+                E3_comp = (stressZ[-1] / strainZ[-1]) / 1e9
+                P13_comp = -(strainX[-1] / strainZ[-1])
+                print('CompZ')
 
         elif loadCase == 'shear':
-            elif loadAxis == 1:
-                G12 = ((stressX[-1] - stressX[0]) / (strainZ[-1] - strainZ[0])) / 1e9
+            if loadAxis == 1:
+                G12 = (stressY[-1] / strainX[-1])
+                print('ShearX')
 
             elif loadAxis == 2:
-                G13 = ((stressY[-1] - stressY[0]) / (strainZ[-1] - strainZ[0])) / 1e9
+                G23 = (stressZ[-1] / strainY[-1])
+                print('ShearY')
 
             elif loadAxis == 3:
-                G23 = ((stressX[-1] - stressX[0]) / (strainY[-1] - strainY[0])) / 1e9
+                G13 = (stressX[-1] / strainZ[-1])
+                print('ShearZ')
 
     # Averaged E&P vales from Tension&Compression:
-    E1, E2, E3 = (E1_tens + E1_comp) / 2, (E2_tens + E2_comp) / 2, (E3_tens + E3_comp) / 2
-    P12, P13, P23 = (P12_tens + P12_comp) / 2, (P13_tens + P13_comp) / 2, (P23_tens + P23_comp) / 2
+    E1, E2, E3 = (E1_tens + abs(E1_comp)) / 2, (E2_tens + abs(E2_comp)) / 2, (E3_tens + abs(E3_comp)) / 2
+    P12, P13, P23 = (P12_tens + abs(P12_comp)) / 2, (P13_tens + abs(P13_comp)) / 2, (P23_tens + abs(P23_comp)) / 2
 
     return E1, E2, E3, P12, P13, P23, G12, G13, G23, E1_tens, E1_comp, E2_tens, E2_comp, E3_tens, E3_comp, P12_tens, P12_comp, P13_tens, P13_comp, P23_tens, P23_comp
 
@@ -150,7 +154,7 @@ if __name__ == '__main__':
     rve_jobs = ['LongitudinalTensionAnalysis', 'LongitudinalCompressionAnalysis', 'LongitudinalShearAnalysis',
                 'TransverseSideTensionAnalysis', 'TransverseSideCompressionAnalysis', 'TransverseSideShearAnalysis',
                 'TransverseTopTensionAnalysis', 'TransverseTopCompressionAnalysis', 'TransverseTopShearAnalysis']
-    rveModel_list = ['RVE1', 'RVE2', 'RVE3']
+    rveModel_list = ['RVE1']
 
     comp_jobs = ['TensionAnalysis', 'CompressionAnalysis', 'ShearAnalysis']
     compModel_list = ['iNSERT ALL MODEL NAMES!']
